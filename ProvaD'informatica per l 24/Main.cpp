fa410f2e6f8853be617bdc7deaad95631b4d84c7
Luca Bigoni <lbigoni@apparound.it>
2022-08-10 19:15:36 +0200

    nuovo programma

---
+++ ProvaD'informatica per l 24/Main.cpp
@@ -0,0 +1,31 @@
+// ProvaD'informatica per l 24.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+/*
+
+Esercizio 
+Il terminal container TDT ha bisogno di un sistema informatico per la gestione dei
+posti DEI CONTAINER.
+Si devono conoscere per ogni CONTAINER il suo codice , provenienza,destinazione, dimensione, costo giornaliero di giacenza in euro e numero di giorni in giacenza.
+
+Il sistema deve mettere a disposizione le seguenti funzionalità:
+
+• caricare la lista dei cotainer
+• visualizzare l’elenco dei container
+• ordinare l’elenco dei posti barca per codice
+• visualizzare i dati di un posto specifico, dato il suo codice in input da tastiera
+• visualizzare l’elenco dei conatiner che arrivano dall'africa
+• Il costo che di dovrà sostenere per uno specifico contaner (si cerca per codice e si effettua il clalcolo (giorni di giacenza *costo giornaliero)
+• salvataggio in un secodo file chiamato "Copia Bk.txt"
+
+Scrivere una funzione distinta per ognuna delle funzionalità richieste scegliendo, opportunamente i parametri da passare.
+Scrivere un programma C++ con menu che permetta di utilizzare tutte le funzionalità sopra elencate.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
